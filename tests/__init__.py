2b4ad8215ebf74514a10154dc8c9b2455dd2f783
bs15 <bs15@sanger.ac.uk>
2023-01-19 11:48:50 +0000

    Tidy tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -14,15 +14,9 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
-import unittest
 import os
 import sys
 
 sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
-import src
-
-
-def suite():
-    return unittest.TestLoader().discover("satlib_app.tests", pattern="*.py")
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'))
+)
